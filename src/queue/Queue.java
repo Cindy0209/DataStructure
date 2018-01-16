ff39a235bdce72c0b9a465453f8588e1e3ff8cbd
Cindy <hmt6688@163.com>
2018-01-16 10:25:52 +0800

    初次提交代码-韩梦甜-2018年1月16日10:11:24

--- src/queue/Queue.java
+++
@@ -1,7 +0,0 @@
-package queue;
-
-/**
- * Created by Cindy on 1/16/2018.
- */
-public interface Queue {
-}
--- src/stack/Stack.java
+++ src/stack/Stack.java
@@ -1,8 +1,27 @@
 package stack;
 
 /**
- * Created by Cindy on 1/16/2018.
+ * 栈接口（出栈，入栈，栈空，栈大小，栈顶）
+ *
+ * @author Cindy
+ * @create 2018-01-16 10:17
  */
 public interface Stack {
+
+	//出栈
+	public Object pop() throws ExceptionStackEmpty;
+
+	//入栈
+	public void push(Object obj);
+
+	//判断栈空
+	public boolean isEmpty();
+
+	//栈大小
+	public int getSize();
+
+	//栈顶
+	public Object top() throws ExceptionStackEmpty;
+
 
 }

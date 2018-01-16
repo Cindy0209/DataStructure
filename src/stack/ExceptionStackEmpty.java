a7f84da1089e3d7e9ed0be1b5e9ff3cdb441fcca
Cindy <hmt6688@163.com>
2018-01-16 10:27:49 +0800

    提交栈异常类-韩梦甜-2018年1月16日10:11:24

---
+++ src/stack/ExceptionStackEmpty.java
@@ -0,0 +1,13 @@
+package stack;
+
+/**
+ * 试图对空栈进行取栈顶或者出栈的异常方法
+ *
+ * @author Cindy
+ * @create 2018-01-16 10:20
+ */
+public class ExceptionStackEmpty extends RuntimeException{
+	public ExceptionStackEmpty(String err){
+		super(err);
+	}
+}
